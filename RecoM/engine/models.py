dcefe52712d3aa8cf573a5adf79a327364d33f6b
Nihar Kashyap <nihar1kashyap7@gmail.com>
2021-05-23 09:02:54 +0530

    A working version

--- RecoM/engine/models.py
+++ RecoM/engine/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Movies(models.Model):
+    movie_title= models.CharField(max_length=100)
+    movie_poster= models.URLField(max_length=256)
+
+class Ratings(models.Model):
+    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(to=Movies, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+

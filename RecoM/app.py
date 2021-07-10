4eb56739a722bf3b9063b2e6d71e7b489edeec0f
Nihar Kashyap <nihar1kashyap7@gmail.com>
2021-07-11 01:21:04 +0530

    Data ingestion started

--- RecoM/app.py
+++ RecoM/app.py
@@ -1,10 +1,22 @@
+'''
+Author: Nihar Kashyap
+Changelog:
+Date: 11/7/2-21
+Time: 1:19 AM
+Changes: Design Changes, Reading movie user feedback csv
+
+'''
+
+
 from flask import Flask
 from flask import render_template
+from flask.globals import session
 import pandas as pd
 import numpy as np
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
 
+from flask import Flask, redirect, url_for, request
 
 tmdb = TMDb()
 tmdb.api_key = '3a9f5aa1d4b3eebd503a6bb3e02cf23f'
@@ -41,14 +53,22 @@ class Predict:
 
 
 app = Flask(__name__)
+
+app.secret_key = "abc"  
+
 df = pd.read_csv('main_data.csv')
+user_mov = pd.read_csv('user_movie.csv') 
 mat = np.load('Sim.npy')
 
 @app.route('/')
 def index():
     movie = Movie()
-        
+    if 'username' in session:
+        print(session['username'])    
     data = list(df['movie_title'])
+    
+    
+    #print(user_mov[user_mov['User']==session['username']])
 
     #for title in data:
         #search = movie.search(title)
@@ -66,3 +86,13 @@ def result(name):
     data = {'name':name, 'arr':arr}
     
     return render_template('result.html', data = data)
+
+@app.route('/login',methods = ['POST', 'GET'])
+def user_login():
+    if request.method == 'POST':
+      user = request.form['username']
+      session['username'] = user
+      return redirect(url_for('index'))
+    else:
+      return render_template('login.html')
+    
--- RecoM/static/js/home.js
+++ RecoM/static/js/home.js
@@ -4,29 +4,38 @@ function search(films)
     var i,x;
     x = document.getElementById("searchBox").value;
     document.getElementById("searchResult").innerHTML="";
+    
+    var count=0;
 
     if(x.length>2)
     {
+    
         for(i=0;i<films.length;i++)
-    {
-        if(films[i].substr(0, x.length).toUpperCase() == x.toUpperCase())
         {
-            var anchor = document.createElement("a");
-            anchor.href = "/result/" + films[i];
-            var textnode = document.createTextNode(films[i]);
-            anchor.appendChild(textnode);
-            var node = document.createElement("Button");                 // Create a <li> node
-            //var textnode = document.createTextNode(films[i]);         // Create a text node
-            //node.appendChild(textnode);
-            node.appendChild(anchor);                              // Append the text to <li>
-            document.getElementById("searchResult").appendChild(node);     // Append <li> to <ul> with id="myList"
-            console.log('found');
+            if(films[i].substr(0, x.length).toUpperCase() == x.toUpperCase())
+            {
+                var anchor = document.createElement("a");
+                anchor.href = "/result/" + films[i];
+                var textnode = document.createTextNode(films[i].toUpperCase());
+                anchor.appendChild(textnode);
+                var node = document.createElement("Button");              
+                var image = document.createElement("img");
+                image.src = "/static/images/movie.jpg";
+                node.appendChild(image);
+                node.appendChild(anchor);                              
+                document.getElementById("searchResult").appendChild(node);
+                count++;     
+                if(count==10)
+                {
+                    break;
+                }
+                console.log(count);
+            }
+            else
+            {
+                console.log('Not found');
+            }
         }
-        else
-        {
-            console.log('Not found');
-        }
-    }
     }
 
     
